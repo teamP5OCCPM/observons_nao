6d928761f1331a989f8f6ad84456656590992cb6
scoomdev <soetaert.chris@gmail.com>
2017-09-05 11:25:12 +0200

    Firt commit
    init symfony project with bootsrap 4 and gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp'),
+    phpcs = require('gulp-phpcs'),
+    sass = require('gulp-ruby-sass'),
+    cleanCSS = require('gulp-clean-css');
+
+gulp.task('psr-2', () =>
+gulp.src(['src/**/*.php', '!vendor/**/*.*', '!node_modules/**/*.php'])
+    .pipe(phpcs({
+        bin: 'vendor/bin/phpcs',
+        standard: 'PSR2',
+        warningSeverity: 0
+    }))
+    .pipe(phpcs.reporter('log'))
+);
+
+gulp.task('sass', () =>
+sass('tw_bootstrap/scss/*.scss')
+    .on('error', sass.logError)
+    .pipe(gulp.dest('app/Resources/css'))
+);
+
+gulp.task('minify-css', ['sass'], () =>
+gulp.src('app/Resources/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('web/css'))
+);
+
+
+gulp.task('default', ['psr-2', 'minify-css']);
+
+gulp.task('watch', () => {
+    gulp.watch('tw_bootstrap/scss/*.scss', ["sass", "minify-css"]).on('change', (event) => {
+        console.log('Le fichier ' + event.path + ' a ete modifier');
+    })
+});
