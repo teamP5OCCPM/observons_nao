a6575ed0a36bccb6c2f0b340f22fbae9055b1f2e
scoomdev <soetaert.chris@gmail.com>
2017-10-19 16:50:38 +0200

    Requête perso limit 4 indexpage
    Requêtes perso mises en place pour les observations et articles visible sur la page d'accueil.

--- src/AppBundle/Controller/CoreController.php
+++ src/AppBundle/Controller/CoreController.php
@@ -4,19 +4,20 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class CoreController extends Controller
 {
     /**
+     * @return Response
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction() : Response
     {
         $em = $this->getDoctrine()->getManager();
-        $observations = $em->getRepository('AppBundle:Observation')->findAll();
-        $articles = $em->getRepository('AppBundle:Article')->findAll();
+        $observations = $em->getRepository('AppBundle:Observation')->getIndexObservations(4);
+        $articles = $em->getRepository('AppBundle:Article')->getIndexArticles(4);
 
         return $this->render('core/index.html.twig', ['observations' => $observations, 'articles' => $articles]);
     }
@@ -24,10 +25,10 @@ class CoreController extends Controller
     /**
      * @param $slug
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      * @Route("/observation/{slug}", name="showObservation")
      */
-    public function showObservationAction($slug)
+    public function showObservationAction($slug) : Response
     {
         $em = $this->getDoctrine()->getManager();
         $observation = $em->getRepository('AppBundle:Observation')->findOneBySlug($slug);
@@ -36,9 +37,12 @@ class CoreController extends Controller
     }
 
     /**
+     * @param Request $request
+     *
+     * @return Response
      * @Route("/les-observations", name="observations")
      */
-    public function observationsAction(Request $request)
+    public function observationsAction(Request $request) : Response
     {
         $em = $this->getDoctrine()->getManager();
         $observations = $em->getRepository('AppBundle:Observation')->findAll();
@@ -47,33 +51,45 @@ class CoreController extends Controller
     }
 
     /**
+     * @param Request $request
+     *
+     * @return Response
      * @Route("/ajouter-observation", name="addObservation")
      */
-    public function addObservationAction(Request $request)
+    public function addObservationAction(Request $request) : Response
     {
         return $this->render('core/addObservation.html.twig');
     }
 
     /**
+     * @param Request $request
+     *
+     * @return Response
      * @Route("/contact", name="contact")
      */
-    public function contactAction(Request $request)
+    public function contactAction(Request $request) : Response
     {
         return $this->render('core/contact.html.twig');
     }
 
     /**
+     * @param Request $request
+     *
+     * @return Response
      * @Route("/qui-sommes-nous", name="who")
      */
-    public function whoAction(Request $request)
+    public function whoAction(Request $request) : Response
     {
         return $this->render('core/who.html.twig');
     }
 
     /**
+     * @param Request $request
+     *
+     * @return Response
      * @Route("/pourquoi-ce-site", name="why")
      */
-    public function whyAction(Request $request)
+    public function whyAction(Request $request) : Response
     {
         return $this->render('core/why.html.twig');
     }
