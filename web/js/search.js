addf919cb872e8081e58abfa44fc088da27df1c9
Sjillen <thomas.proust@outlook.com>
2017-10-27 08:30:17 +0900

    modification id filtre de l'index et ajout de typeahead dans searchbar
    Il y a un petit bug d'affichage si on utilise le selecteur class filter-search pour faire fonctionner typeahead. A voir pour faire marcher typeahead séparément en fonction d'une barre de recherche à l'autre

--- web/js/search.js
+++ web/js/search.js
@@ -11,28 +11,37 @@ $(document).ready(function(){
     
         // Constructing the suggestion engine
     
-       console.log(birds);
+        var places = new BloodHound({
+            
+        })
     
         
     
         // Initializing the typeahead
+    $('.filter-search').on('change', function() {
+        if (this.value === "species") {
+            $('.typeahead').typeahead({
+            
+                    hint: true,
+            
+                    highlight: true, /* Enable substring highlighting */
+            
+                    minLength: 1 /* Specify minimum characters required for showing suggestions */
+            
+                },
+            
+                {
+            
+                    name: 'birds',
+                    display: 'species',
+                    source: birds
+            
+                });
+        }else{
+            $('.typeahead').typeahead('destroy');
+            console.log('mauvais filtre');
+        } 
+    }); 
+     
     
-        $('.typeahead').typeahead({
-    
-            hint: true,
-    
-            highlight: true, /* Enable substring highlighting */
-    
-            minLength: 1 /* Specify minimum characters required for showing suggestions */
-    
-        },
-    
-        {
-    
-            name: 'birds',
-            display: 'species',
-            source: birds
-    
-        });
-    
-    });  
+});
